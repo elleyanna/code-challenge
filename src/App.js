89884eba71d1a670d9793affbed93470536e814a
Elleanna Dwyer <e.dwyer@printfly.com>
2020-07-09 10:10:14 -0400

    Added final comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ResidentNameInput from "./components/ResidentNameInput";
 import UnitInput from "./components/UnitInput.js";
+import Typography from "@material-ui/core/Typography";
 import { property_data } from "./data/PropertyData.js";
 import "./App.css";
 
 function App() {
   return (
+    // Given more time I would create a prettier homepage display right now it's just barebones requirements
     <div className="App">
       <UnitInput
         data={property_data.people}
