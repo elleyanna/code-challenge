d7a435808a5f0041f264735c067266c284f3fb02
Elleanna Dwyer <e.dwyer@printfly.com>
2020-07-09 06:20:07 -0400

    Updated main files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ResidentNameInput from "./components/ResidentNameInput";
+import UnitInput from "./components/UnitInput.js";
+import { property_data } from "./data/PropertyData.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UnitInput
+        data={property_data.people}
+        options={[
+          ...new Set(
+            property_data.people.map((person) => {
+              return person.unit;
+            })
+          ),
+        ]}
+      />
+      <ResidentNameInput data={property_data} options={property_data.people} />
     </div>
   );
 }
